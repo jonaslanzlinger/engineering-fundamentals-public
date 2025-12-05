563d1be4296fcfd611fd288288fd8d1a197007f9
Jonas Länzlinger <141398686+jonaslanzlinger@users.noreply.github.com>
2025-12-05 09:28:36 +0000

    ft: create new workflow and tests

--- src/__tests__/Counter.test.tsx
+++ src/__tests__/Counter.test.tsx
@@ -1,7 +1,18 @@
-import { render} from "@testing-library/react";
+import { render, screen, fireEvent} from "@testing-library/react";
 import Counter from "../Counter";
 
 test("increments count when button is clicked", () => {
     render(<Counter />);
     // Add the test logic below
+
+    // Find the button
+    const button = screen.getByRole("button", { name: /count is 0/i });
+
+    // Click once → should be 1
+    fireEvent.click(button);
+    expect(button).toHaveTextContent("count is 1");
+
+    // Click again → should be 2
+    fireEvent.click(button);
+    expect(button).toHaveTextContent("count is 2");
 });
